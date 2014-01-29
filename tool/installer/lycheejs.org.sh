a523ddb119a249d733cbe1ad02f087cd520022a9
Christoph Martens <christoph@martens.ms>
2014-01-29 12:48:02 +0100

    Fixes

--- tool/installer/lycheejs.org.sh
+++ tool/installer/lycheejs.org.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./sorbet.sh install https://github.com/martensms/martensms-website/archive/master.zip martensms-website;
 ./sorbet.sh install https://github.com/LazerUnicorns/lycheeJS-slides/archive/master.zip lycheeJS-slides;
 ./sorbet.sh install https://github.com/LazerUnicorns/lycheeJS-website/archive/master.zip lycheeJS-website;
 
